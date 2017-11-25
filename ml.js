42d8556f8b514c29618c40e0e7f021b02cd0da1f
haack-improbable <alexhaak@improbable.io>
2017-11-25 17:23:06 +0200

    Added feature "extraction" from data

--- ml.js
+++ ml.js
@@ -20,26 +20,13 @@ function retrain() {
     model.train();
 }
 
-function predict(features) {
-    let answers = model.predict(features);
+function predict(data) {
+    let features = dataset.getFeaturesFromData(data);
     let rankings = [];
 
-    for (let i in answers) {
-        if (typeof answers[i] !== 'string') {
-            rankings.push({
-                name: i,
-                rating: answers[i]
-            });
-        }
-    }
-
-    return rankings.sort((a,b) => b.rating - a.rating);
+    return features;
 }
 
 buildModel();
 module.exports.predict = predict;
 module.exports.retrain = retrain;
-
-
-// let answer = cls.predict(['football', 24, 1, 'sushi', 'coffee', 'cocktail', 'rave', 'beach']);
-// let answer = cls.predict(['football', 1, 1, 'kebab', 'milk', null, 'rave', null]);
--- server.js
+++ server.js
@@ -2,25 +2,12 @@ const express = require('express');
 const app = express();
 const model = require('./ml');
 
-let top = model.predict([null, null, null, null, null, null, 'alt_rock', null]);
-console.log(top);
+app.get('/predict', (req, res) => {
+  //call python script with data
+  console.log('Making prediction...');
+  let recs = model.predict(req.data);
 
-// const locations = [
-//   {
-//     name: "london",
-//     finnairId: 123,
-//   },
-// ]
-//
-// app.get('/locations', (req, res) => res.send(locations))
-//
-// app.get('/answer', (req, res) => console.log('nice'))
-//
-// app.get('/predict', (req, res) => {
-//   //call python script with data
-//   let recs = [{...locations, rating: 0.5}];
-//
-//   res.send(recs)
-// })
-//
-// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  res.send(recs);
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
